13402ba7bc1bac8cf654f19024ef20714393fe57
Anna <annazanotti49@gmail.com>
2019-04-06 11:42:42 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'pages/info'
+	root to: redirect('/ideas') # muda a root para o ideas e não rails girls
+  resources :ideas
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
